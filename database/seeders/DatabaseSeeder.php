1c09647c34dfab76aee3b94a0dbf794996cedbfb
robert <robert.wojcik.programista@gmail.com>
2024-03-15 20:22:27 +0100

    implemented user registration and login

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\ExpensesCategoryDefault;
+use App\Models\IncomesCategoryDefault;
+use App\Models\PaymentMethodsDefault;
+use Database\Factories\IncomesCategoryDefaultFactory;
+use Database\Factories\PaymentMethodsDefaultFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +17,83 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+//         \App\Models\User::factory(5)->create();
+ //       \App\Models\User::factory()->create([
+//             'username' => 'Test User',
+//             'email' => 'test@example.com',
+//             'password' => 'qwerty'
+//         ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+//        PaymentMethodsDefault::factory()->create([
+//            'name' => 'Cash'
+//          ]);
+//        PaymentMethodsDefault::factory()->create([
+//            'name' => 'Debit Card'
+//        ]);
+//        PaymentMethodsDefault::factory()->create([
+//            'name' => 'Credit Card'
+//        ]);
+
+//        IncomesCategoryDefault::factory()->create([
+//            'name' => 'Salary'
+//        ]);
+//        IncomesCategoryDefault::factory()->create([
+//            'name' => 'Interest'
+//        ]);
+//        IncomesCategoryDefault::factory()->create([
+//            'name' => 'Allegro'
+//        ]);
+//        IncomesCategoryDefault::factory()->create([
+//            'name' => 'Another'
+//        ]);
+
+//        ExpensesCategoryDefault::factory()->create([
+//           'name' => 'Transport'
+//        ]);
+//        ExpensesCategoryDefault::factory()->create([
+//            'name' => 'Books'
+//        ]);
+//        ExpensesCategoryDefault::factory()->create([
+//            'name' => 'Food'
+//        ]);
+//        ExpensesCategoryDefault::factory()->create([
+//            'name' => 'Apartments'
+//        ]);
+//        ExpensesCategoryDefault::factory()->create([
+//            'name' => 'Telecommunication'
+//        ]);
+//        ExpensesCategoryDefault::factory()->create([
+//            'name' => 'Health'
+//        ]);
+//        ExpensesCategoryDefault::factory()->create([
+//            'name' => 'Clothes'
+//        ]);
+//        ExpensesCategoryDefault::factory()->create([
+//            'name' => 'Hygiene'
+//        ]);
+//        ExpensesCategoryDefault::factory()->create([
+//            'name' => 'Kids'
+//        ]);
+//        ExpensesCategoryDefault::factory()->create([
+//            'name' => 'Recreation'
+//        ]);
+//        ExpensesCategoryDefault::factory()->create([
+//            'name' => 'Trip'
+//        ]);
+//        ExpensesCategoryDefault::factory()->create([
+//            'name' => 'Savings'
+//        ]);
+//        ExpensesCategoryDefault::factory()->create([
+//            'name' => 'For retirement'
+//        ]);
+//        ExpensesCategoryDefault::factory()->create([
+//            'name' => 'Debt repayment'
+//        ]);
+//        ExpensesCategoryDefault::factory()->create([
+//            'name' => 'Gift'
+//        ]);
+//        ExpensesCategoryDefault::factory()->create([
+//            'name' => 'Another'
+//        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/add-income', function () {
+    return view('add-income');
+})->middleware(['auth', 'verified'])->name('addIncome');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
